c94d64163f40e1086b5bdb5522127967e639e6aa
Navid SP <37252168+glad-tidings@users.noreply.github.com>
2024-10-02 08:02:12 +0330

    Add files via upload

---
+++ PEVM.vb
@@ -0,0 +1,14 @@
+﻿Public Class PEVM
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        Dim myProcess As Process = New Process()
+        myProcess.StartInfo.FileName = "C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"
+        myProcess.StartInfo.Arguments = "-T ws start ""C:\Users\Glad Tidings\Documents\Virtual Machines\PEVoip\PEVoip.vmx"""
+        myProcess.StartInfo.CreateNoWindow = True
+        myProcess.Start()
+        'Shell("vmrun -T ws start ""C:\Users\Glad Tidings\Documents\Virtual Machines\PEVoip\PEVoip.vmx""", AppWinStyle.Hide)
+    End Sub
+
+    Protected Overrides Sub OnStop()
+
+    End Sub
+End Class
---
+++ ProjectInstaller.Designer.vb
@@ -0,0 +1,47 @@
+﻿<System.ComponentModel.RunInstaller(True)> Partial Class ProjectInstaller
+    Inherits System.Configuration.Install.Installer
+
+    'Installer overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Component Designer
+    'It can be modified using the Component Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.PEVMServiceProcess = New System.ServiceProcess.ServiceProcessInstaller()
+        Me.PEVMService = New System.ServiceProcess.ServiceInstaller()
+        '
+        'PEVMServiceProcess
+        '
+        Me.PEVMServiceProcess.Password = Nothing
+        Me.PEVMServiceProcess.Username = Nothing
+        '
+        'PEVMService
+        '
+        Me.PEVMService.Description = "Pars Elecom Company"
+        Me.PEVMService.DisplayName = "Pars Elecom VM Agent"
+        Me.PEVMService.ServiceName = "PEVM"
+        Me.PEVMService.StartType = System.ServiceProcess.ServiceStartMode.Automatic
+        '
+        'ProjectInstaller
+        '
+        Me.Installers.AddRange(New System.Configuration.Install.Installer() {Me.PEVMServiceProcess, Me.PEVMService})
+
+    End Sub
+
+    Friend WithEvents PEVMServiceProcess As ServiceProcess.ServiceProcessInstaller
+    Friend WithEvents PEVMService As ServiceProcess.ServiceInstaller
+End Class
